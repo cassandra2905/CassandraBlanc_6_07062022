44614247f242f256da0b8fba84fdcd91f08a095a
Cassandra <c.blanc97fleur@gmail.com>
2022-07-04 21:43:33 +0200

    implémenations finales

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,7 @@ exports.signup = (req, res, next) => {
                 user
                     .save()
                     .then(() => res.status(201).json({ message: 'Utilisateur créé ! ' }))
-                    .catch(error => res.status(400).json({ error }));
+                    .catch(error => res.status(400).json({ error: "Erreur à l'enregistrement" }));
             } else {
                 res.status(400).json({ error: "Adresse mail invalide" });
             }
